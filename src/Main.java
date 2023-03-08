a547ab2a5b70e633567df9642220474d72da6ade
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-03-08 21:33:00 +0100

    Add solution

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*Задача 2
 Создайте класс Triangle (треугольник), принимающий три целых значения - длины трёх сторон
 треугольника. В конструкторе нужно проверить введённые значения на корректность (например,
@@ -7,7 +11,13 @@
  конструкторе.
 При провале любой из проверок нужно вывести на экран соответствующее сообщение.*/
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int a = Integer.parseInt(br.readLine());
+    int b = Integer.parseInt(br.readLine());
+    int c = Integer.parseInt(br.readLine());
+    Triangle newTriangle = new Triangle(a, b, c);
+
+
   }
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -7,4 +7,47 @@
  конструкторе.
 При провале любой из проверок нужно вывести на экран соответствующее сообщение.*/
 public class Triangle {
+  int a, b, c;
+
+  public Triangle(int a, int b, int c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    checkSides(a, b, c);
+  }
+  public static boolean checkSides(int a, int b, int c) {
+    if (!minus(a, b, c)) {
+      return false;
+
+    } else {
+      if ((b <= c + a) || (a <= b + c) || (c <= b + a)) {
+        System.out.println("Неравенство выполняется");
+        return true;
+      } else {
+        System.out.println("Error");
+        return false;
+      }
+    }
+  }
+  public static boolean minus(int a, int b, int c) {
+    String error;
+    if (a < 0 || b < 0 || c < 0) {
+      if (a < 0) {
+        error = "Ошибка А =" + a + "<0 ";
+        System.out.println(error);
+      }
+      if (b < 0) {
+        error = "Ошибка B =" + b + "<0 ";
+        System.out.println(error);
+      }
+      if (c < 0) {
+        error = "Ошибка C =" + c + "<0 ";
+        System.out.println(error);
+      }
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
+
